e437e102dc9ed7bfa0741f2d11707c3a42cbc3c9
Al Raju Abdullah <abdullahalraju33@gmail.com>
2026-04-30 18:36:17 +0600

    Change adapter from Node to Vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,12 @@
-import adapter from "@sveltejs/adapter-node";
-import { preprocessMeltUI } from "@melt-ui/pp";
-import sequence from "svelte-sequential-preprocessor";
-import preprocess from "svelte-preprocess";
+import adapter from '@sveltejs/adapter-vercel';
+import { preprocessMeltUI } from '@melt-ui/pp';
+import sequence from 'svelte-sequential-preprocessor';
+import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter({
-      out: 'build',
-      precompress: true,
-      envPrefix: 'APP_',
-      polyfill: true,
-      external: [],
-    }),
-    csrf: {
-      checkOrigin: true,
-    },
+    adapter: adapter()
   },
   preprocess: sequence([
     preprocess({
